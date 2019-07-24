f4ff308c79b776a144d222e445a74bbfd657c8a2
chrismastrogiovanni11 <chrismaastro@gmail.com>
2019-07-24 07:47:53 -0500

    init commit part two

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+   public class BinarySearchTree
+    {
+        
+        public Node root;
+        //LinkedList<int> binaryList = new LinkedList<int>();
+
+
+
+        public BinarySearchTree()
+        {
+            
+        }
+
+        public void Add(int number)
+        {
+
+           //Console.WriteLine("enter number");
+            //number = int.Parse(Console.ReadLine());
+            
+
+        }
+    }
+
+}
